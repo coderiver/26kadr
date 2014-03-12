b00fe2a33f074e567daa43f9341bfa7ad4413eab
moroz <morozarturv@gmail.com>
2014-03-12 17:44:46 +0200

    added all navigation

--- js/common.js
+++ js/common.js
@@ -4,11 +4,17 @@ function nav () {
 	var item = $('.step');
 	var btn = $('.js-next');
 	btn.on('click', function(){
-		var el = $(this).attr('href');
+		var attr = $(this).attr('href');
 		item.removeClass('is-active');
 		btn.removeClass('is-active');
-		$('#'+el).addClass('is-active');
+		$('#'+attr).addClass('is-active');
 		$(this).addClass('is-active');
+		btn.each(function(){
+			var btn_attr = $(this).attr('href');
+			if (attr == btn_attr) {
+				$(this).addClass('is-active');
+			};
+		});
 		return false;
 	});
 }
