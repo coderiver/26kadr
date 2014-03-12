f4b5acf9d27b829fc704c75826fd63dbdf5e07d5
moroz <morozarturv@gmail.com>
2014-03-12 17:23:03 +0200

    without animation and sidebar navigation

--- js/common.js
+++ js/common.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
 
+function nav () {
+	var item = $('.step');
+	var btn = $('.js-next');
+	btn.on('click', function(){
+		var el = $(this).attr('href');
+		item.removeClass('is-active');
+		$('#'+el).addClass('is-active');
+		return false;
+	});
+}
+nav();
+
 });
