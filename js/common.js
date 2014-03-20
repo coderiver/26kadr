e2aaf8375a39a67845d9a9700dffa4572028437a
moroz <morozarturv@gmail.com>
2014-03-20 15:23:34 +0200

    fixes

--- js/common.js
+++ js/common.js
@@ -2,9 +2,27 @@ $(document).ready(function() {
 
 // datepicker
 var dp = $('.js-dp');
+$.datepicker.regional['ru'] = { 
+	closeText: 'Закрыть', 
+	prevText: '&#x3c;Пред', 
+	nextText: 'След&#x3e;', 
+	currentText: 'Сегодня', 
+	monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь', 
+	'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'], 
+	monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн', 
+	'Июл','Авг','Сен','Окт','Ноя','Дек'], 
+	dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'], 
+	dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'], 
+	dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'], 
+	dateFormat: 'dd.mm.yy', 
+	firstDay: 1, 
+	isRTL: false 
+}; 
+$.datepicker.setDefaults($.datepicker.regional['ru']); 
 if (dp.length) {
 	dp.datepicker({
-		numberOfMonths: 2
+		numberOfMonths: 2,
+		minDate: 1
 	});
 };
 
