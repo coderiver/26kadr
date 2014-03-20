f0677f67ccbb5d7c68d95a258a81306506a3acb0
moroz <morozarturv@gmail.com>
2014-03-20 12:59:43 +0200

    added tooltip

--- js/common.js
+++ js/common.js
@@ -20,4 +20,43 @@ function nav () {
 }
 nav();
 
+
+// datepicker
+var dp = $('.js-dp');
+if (dp.length) {
+	dp.datepicker({
+		numberOfMonths: 2
+	});
+};
+
+// tooltip
+function tooltip () {
+	var el = $('.js-ttip'),
+			template = $('.js-ttip-template'),
+			template_text = template.find('.ttip__text'),
+			template_link = template.find('.ttip__load a'),
+			template_size = template.find('.ttip__load span');
+	el.hover(function(){
+		var text = $(this).attr('data-text'),
+				link = $(this).attr('data-link'),
+				size = $(this).attr('data-size'),
+				top = $(this).offset().top,
+				left = $(this).offset().left,
+				el_height = $(this).height();
+		template_text.html(text);
+		template_link.attr('href', link);
+		template_size.html(size);
+		template.show();
+		var height = template.height();
+		template.css({'top': top - height - el_height + 10, 'left': left});
+		template
+	}, function(){
+		//template.hide();
+	});
+	template.hover(function(){},function(){
+		$(this).hide();
+	})
+}
+tooltip();
+
 });
